dd3d6b4f10f6d0b4f4340ebe79a76469e36e5aa4
Chakravarthy-7 <chakravarthy.8328@gmai.com>
2023-06-02 13:20:59 +0530

    added some functionalities

--- src/components/Post/New/NewPost.js
+++ src/components/Post/New/NewPost.js
@@ -1,37 +1,59 @@
-import React, { useState } from "react";
-import icon from "../../../images/icon.png";
-import camera from "../../../images/camera.png";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
+import icon from "../../../images/icon.png"
+import  camera  from "../../../images/camera.png"
 import {Link} from "react-router-dom"
-import NewForm from "./NewForm";
 
 const NewPost = () => {
-  const navigate = useNavigate();
-  const [file, setFile] = useState(null);
-  const [author, setAuthor] = useState("");
-  const [location, setLoaction] = useState("");
-  const [discription, setDiscription] = useState("");
-
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-  };
-
-  const [formData, setFormData] = useState({
-    image: null,
-    author: "",
-    location: "",
-    description: "",
+  const [files, setFiles] = useState(() => {
+    const storedFiles = localStorage.getItem('uploadedFiles');
+    return storedFiles ? JSON.parse(storedFiles) : [];
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formValues = new FormData(e.target);
-    navigate("/postview/allposts");
+  const [formData, setFormData] = useState(() => {
+    const storedData = localStorage.getItem('formData');
+    return storedData
+      ? JSON.parse(storedData)
+      : { name: '', location: '', description: '' };
+  });
+
+  const onDrop = (acceptedFiles) => {
+    setFiles([...files, ...acceptedFiles]);
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleDelete = (index) => {
+    setFiles((prevFiles) => {
+      const updatedFiles = [...prevFiles];
+      updatedFiles.splice(index, 1);
+      return updatedFiles;
+    });
+  };
+
+  const handleAddPost = () => {
+    const newPost = {
+      file: null,
+      name: formData.name,
+      location: formData.location,
+      description: formData.description,
+    };
+    setFiles([...files, newPost]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('uploadedFiles', JSON.stringify(files));
+  }, [files]);
+
+  useEffect(() => {
+    localStorage.setItem('formData', JSON.stringify(formData));
+  }, [formData]);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className="">
+    <div>
       <nav className="flex justify-between m-9">
         <div className="flex">
           <img
@@ -42,100 +64,106 @@ const NewPost = () => {
           <h3 className="text-green-600 text-3xl">Instagram Clone</h3>
         </div>
         <Link to="/postview/newform">
-        <img
-          src={camera}
-          alt="camera"
-          className="w-[40px] h-[50px] object-cover cursor-pointer"
-        />
+          <img
+            src={camera}
+            alt="camera"
+            className="w-[40px] h-[50px] object-cover cursor-pointer"
+          />
         </Link>
       </nav>
       <hr className="bg-black" />
 
-      <div className="posts flex justify-center ml-[400px] flex-col">
-        <div class="bg-gray-100 p-4">
-          <div class="bg-white border rounded-sm max-w-md">
-            <div class="flex items-center px-4 py-3">
-              <img
-                class="h-8 w-8 rounded-full"
-                src="https://picsum.photos/id/1027/150/150"
-              />
-              <div class="ml-3 ">
-                <span class="text-sm font-semibold antialiased block leading-tight">
-                  8fact
-                </span>
-                <span class="text-gray-600 text-xs block">
-                  Asheville, North Carolina
-                </span>
-              </div>
-            </div>
-            <img src="https://picsum.photos/id/244/900/900" />
-            <div class="flex items-center justify-between mx-4 mt-3 mb-2">
-              <div class="flex gap-5">
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-                </svg>
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path
-                    clip-rule="evenodd"
-                    d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z"
-                    fill-rule="evenodd"
-                  ></path>
-                </svg>
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
-                </svg>
-              </div>
-              <div class="flex">
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z"></path>
-                </svg>
-              </div>
-            </div>
-            <div class="font-semibold text-sm mx-4 mt-2 mb-4">92,372 likes</div>
-          </div>
+      <div className="max-w-md mx-auto mt-3 border border-gray-500 p-4 rounded-lg">
+        <div
+          {...getRootProps({ className: 'dropzone' })}
+          className="border border-black rounded-md p-2 mb-1"
+        >
+          <input {...getInputProps()} />
+          <p>Drag and drop files here, or click to select files</p>
         </div>
-        <div class="bg-gray-100 p-4">
-          <div class="bg-white border rounded-sm max-w-md">
-            <div class="flex items-center px-4 py-3">
-              <img
-                class="h-8 w-8 rounded-full"
-                src="https://picsum.photos/id/1027/150/150"
-              />
-              <div class="ml-3 ">
-                <span class="text-sm font-semibold antialiased block leading-tight">
-                  8fact
-                </span>
-                <span class="text-gray-600 text-xs block">
-                  Asheville, North Carolina
-                </span>
-              </div>
-            </div>
-            <img src="https://picsum.photos/id/244/900/900" />
-            <div class="flex items-center justify-between mx-4 mt-3 mb-2">
-              <div class="flex gap-5">
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-                </svg>
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path
-                    clip-rule="evenodd"
-                    d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z"
-                    fill-rule="evenodd"
-                  ></path>
-                </svg>
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
-                </svg>
-              </div>
-              <div class="flex">
-                <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                  <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z"></path>
-                </svg>
-              </div>
-            </div>
-            <div class="font-semibold text-sm mx-4 mt-2 mb-4">92,372 likes</div>
-          </div>
+        <div className="mb-4">
+          <input
+            className="w-full border border-gray-300 rounded px-3 py-2"
+            type="text"
+            name="name"
+            value={formData.name}
+            placeholder="enter name"
+            onChange={handleChange}
+          />
         </div>
+
+        <div className="mb-4">
+          <input
+            className="w-full border border-gray-300 rounded px-3 py-2"
+            type="text"
+            placeholder="location"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4">
+          <textarea
+            className="w-full border border-gray-300 rounded px-3 py-2"
+            type="text"
+            id="emailInput"
+            placeholder="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          onClick={handleAddPost}
+        >
+          Post
+        </button>
+      </div>
+      <div className="file-preview flex justify-center flex-col-reverse ml-[460px]">
+        {files.map((file, index) => {
+          if (file.type && file.type.startsWith('image/')) {
+            return (
+              <div key={index} className="preview-item w-[300px]">
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt={`File ${index}`}
+                  className="preview-image"
+                />
+                <div className="preview-details">
+                  <p>Name: {formData.name}</p>
+                  <p>Location: {formData.location}</p>
+                  <p>Description: {formData.description}</p>
+                  <button
+                    onClick={() => handleDelete(index)}
+                    className="bg-red-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} className="preview-item">
+                <p>Unsupported file type: {file.name}</p>
+                <div className="preview-details">
+                  <p>Name: {formData.name}</p>
+                  <p>Location: {formData.location}</p>
+                  <p>Description: {formData.description}</p>
+                  <button
+                    onClick={() => handleDelete(index)}
+                    className="bg-red-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
